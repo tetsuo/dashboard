c68bd5bd5d598ab32a06d4f12cafe07ced05b256
tetsuo <ogunduz@gmail.com>
2025-05-27 05:07:09 +0200

    Add base MUI theme

---
+++ muiTheme.js
@@ -0,0 +1,55 @@
+import { createTheme } from '@mui/material/styles'
+
+const muiTheme = createTheme({
+  typography: {
+    htmlFontSize: 16,
+    fontSize: 14,
+    h1: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          padding: '0.62rem 1.2rem',
+          fontWeight: 'bold',
+          fontSize: '1rem',
+          borderRadius: '24px',
+        },
+      },
+    },
+    MuiSwitch: {
+      styleOverrides: {
+        switchBase: {
+          color: '#888888',
+          '&.Mui-checked': {
+            color: '#1066e4',
+          },
+          '&.Mui-checked + .MuiSwitch-track': {
+            backgroundColor: '#7e8af9',
+          },
+        },
+        checked: {},
+        track: {
+          backgroundColor: '#999',
+        },
+      },
+    },
+  },
+  palette: {
+    background: {
+      default: '#fff',
+    },
+    primary: {
+      main: '#000',
+    },
+    secondary: {
+      main: '#0418ef',
+    },
+  },
+})
+
+export default muiTheme
