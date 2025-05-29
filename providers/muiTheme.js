528127f793397c44e1e0711f6c16ff762f23be70
tetsuo <ogunduz@gmail.com>
2025-05-29 09:56:37 +0200

    Move Sidebar and its sub components to drawer namespace

--- providers/muiTheme.js
+++ providers/muiTheme.js
@@ -237,6 +237,52 @@ const theme = createTheme({
         },
       },
     },
+    MuiDrawer: {
+      styleOverrides: {
+        paper: ({ theme }) => ({
+          width: 240,
+          backgroundColor: '#f7f7f7',
+          '.MuiContainer-root:first-of-type': {
+            padding: 0,
+            marginBottom: theme.spacing(2),
+            '.MuiToolbar-root': {
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+              padding: theme.spacing(0), // TODO (0, 1)
+              paddingLeft: theme.spacing(2),
+              minHeight: '64px',
+              '.MuiIconButton-root': {
+                color: 'rgba(0, 0, 0, 0.4)',
+              },
+            },
+          },
+          '.MuiContainer-root:last-of-type': {
+            padding: 0,
+            marginTop: 'auto',
+            marginBottom: theme.spacing(1),
+          },
+          '.MuiList-root': {
+            paddingTop: 0,
+            a: {
+              color: 'rgba(0, 0, 0, 0.6)',
+            },
+            '.MuiListItemIcon-root': {
+              color: 'rgba(0, 0, 0, 0.4)',
+            },
+            '.Mui-selected': {
+              color: 'rgba(0, 0, 0, 0.85)',
+            },
+            '.Mui-selected .MuiListItemIcon-root': {
+              color: 'rgba(0, 0, 0, 0.7)',
+            },
+            '.MuiListItemButton-root': {
+              paddingLeft: theme.spacing(3),
+            },
+          },
+        }),
+      },
+    },
   },
   palette: {
     background: {
