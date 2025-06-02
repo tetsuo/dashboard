5f6c9938ff1bb4b6d75411094e35c3c0f1e1d013
tetsuo <ogunduz@gmail.com>
2025-06-02 09:29:29 +0200

    Fix inconsistent React import

--- components/timeline/Ruler.jsx
+++ components/timeline/Ruler.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import addMinutes from 'date-fns/addMinutes'
 import getMinutes from 'date-fns/getMinutes'
 import getHours from 'date-fns/getHours'
